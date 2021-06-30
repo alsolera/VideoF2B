90a62d62c41902624a0c27f9e85fcc5bd126f108
amv <basil96@users.noreply.github.com>
2021-06-29 23:44:02 -0400

    Refactor: update cam locator script for practical field application
    * Better to calculate a range of cam distances against a few given constraints than to leave it open-ended.  The constraint here is that the camera is placed at a user-specified height relative to the flight sphere's equator.  This calculator determines a range of possible distances for the camera, and the corresponding vertical view angles for each distance.  This allows the operator to start with a desired cam height on a tripod, then using the calculated distance range to place the cam. Such placement guarantees a visible gap between the sphere's visible edge and the 45-degree latitude near the top of the video.  This guarantee is of course contingent on a wide enough field of view of the cam itself.
    
    * Prettify the output, make it easier to find the needed info at a glance.
    
    * Default setup: Sphere R = 21m, cam is 1.0 m below equator.
    
    * Reference section: include angle of view for a 14mm lens on a full-frame sensor.

--- CamLocator.py
+++ CamLocator.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 # CamLocator - calculate the best location for a camera.
-# Copyright (C) 2020  Andrey Vasilik - basil96@users.noreply.github.com
+# Copyright (C) 2020 -2021  Andrey Vasilik - basil96@users.noreply.github.com
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,7 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import argparse as ap
+from math import cos, degrees, pi, radians, sin, tan, sqrt
+
 import numpy as np
+
+import geometry as geom
 
 
 def get_angle_arr(m):
@@ -23,11 +29,7 @@ def get_angle_arr(m):
             v = m[2] - m[0]'''
     u = m[1] - m[0]
     v = m[2] - m[0]
-    u_norm = np.linalg.norm(u)
-    v_norm = np.linalg.norm(v)
-    if u_norm * v_norm == 0.0:
-        return np.nan
-    return np.arccos(u.dot(v) / (u_norm * v_norm))
+    return geom.angle(u, v)
 
 
 def get_tangent_point(p, R):
@@ -38,7 +40,7 @@ def get_tangent_point(p, R):
     det = d**2 - R**2
     if det < 0.:
         raise Exception('Point is inside circle, tangent is undefined')
-    r = R / d * np.sqrt(det)
+    r = R / d * sqrt(det)
     n = p / d
     n_perp = np.array([n[1], -n[0]])  # rotation of n by -90deg
     q = a * n + r * n_perp
@@ -55,32 +57,108 @@ def str_vec(u):
     return f"({','.join(f'{s:.3f}' for s in u)})"
 
 
-def main():
-    R = 15.92  # F2D lines
-    # R = 63. * 0.3048  # 60ft lines C-C plus 3ft of arm
-    # x coord is at intersection of equator and 45° tangent line
-    # p0 = np.array([-R * np.sqrt(2) + 2, -1.])
-    # p0 = np.array([-21.9, -1.])
-    p0 = np.array([-20.3, -1.])
-    # p0 = np.array([-25, -1.])
-    # p0 = np.array([-100.*.3048, -1.])
-    p1 = get_tangent_point(p0, R)
-    p2 = np.array([0., -1.55])
-    P = np.vstack((p0, p1, p2))
-    view_angle = get_angle_arr(P)
+def solve_x(R, h, target_alpha, ground_level):
+    from scipy.optimize import fsolve
+
+    def func(x):
+        p0 = [x[0], h]  # use x[0] because fsolve supplies an array, which throws deprecation warnings here
+        try:
+            p1 = get_tangent_point(p0, R)
+        except:
+            return 1e6
+        p2 = [0, ground_level]
+        P = np.vstack((p0, p1, p2))
+        alpha = get_angle_arr(P)
+        return target_alpha - alpha
+
+    ret = fsolve(func, R+h)
+    return ret[0]
+
+
+def main(R, h, max_alpha, ground_level):
+    '''For a given `h` (signed height relative to equator),
+        a given sphere radius `R`,
+        and a given vertical view angle `max_alpha` in degrees, find:
+            `d_min` and `d_max` such that `alpha <= max_alpha`.
+        Center marker is at `ground_level` relative to equator.'''
+    p2 = [0, ground_level]
+    max_alpha = radians(max_alpha)
+    # 45deg tangent line equation
+    quart_pi = 0.25 * pi
+    b = R * (sin(quart_pi)+cos(quart_pi))
+    # Find `x_max` (where tangent line intersects the camera height)
+    x_max = h - b
+    p0_max = [x_max, h]
+    p1_max = get_tangent_point(p0_max, R)
+    P_max = np.vstack((p0_max, p1_max, p2))
+    alpha_at_max = get_angle_arr(P_max)
+    # `x_min` is at first limited by sphere radius
+    x_min = -R
+    # The other limit for `x_min` is `max_alpha`
+    x_min_for_alpha = solve_x(R, h, max_alpha, ground_level)
+    # Both x_min values are negative, so take the min of the two.
+    x_min = min(x_min, x_min_for_alpha)
+    # Use absolute value for the user because it's just a distance.
+    d_min = abs(x_min)
+    d_max = abs(x_max)
+    # Output
+    d_max_str = f'{d_max:.2f}'
+    alpha_at_max_str = f'{degrees(alpha_at_max):.2f}°'
+    elev_max_str = f'{degrees(get_elevation_angle(p1_max)):.2f}°'
+    if d_min <= d_max:
+        p0_min = [x_min, h]
+        p1_min = get_tangent_point(p0_min, R)
+        P_min = np.vstack((p0_min, p1_min, p2))
+        alpha_at_min = get_angle_arr(P_min)
+        d_min_str = f'{d_min:.2f}'
+        alpha_at_min_str = f'{degrees(alpha_at_min):.2f}°'
+        elev_min_str = f'{degrees(get_elevation_angle(p1_min)):.2f}°'
+    else:
+        d_min_str = alpha_at_min_str = elev_min_str = 'N/A'
+    w = 8
     print('=' * 80)
-    print(f'              Sphere radius R = {R}')
-    print(f'              Camera location = {str_vec(p0)}')
-    print(f'   Tangent from cam to sphere = {str_vec(p1)}')
-    print(f"Sphere center at pilot's feet = {str_vec(p2)}")
-    print(f'           Minimum view angle = {np.degrees(view_angle):.2f}°')
-    print(f'   Elevation angle at tangent = {np.degrees(get_elevation_angle(p1)):.2f}°')
+    print('--- INPUTS ----')
+    print(f'                Sphere radius R = {R:.2f}')
+    print(f'      Camera height wrt equator = {h:.2f}')
+    print(f'       Ground level wrt equator = {ground_level:.2f}')
+    print(f'    Maximum vertical view angle = {degrees(max_alpha):.2f}°')
+    print('--- RESULTS ---')
+    print(f'                   |---------------------|')
+    print(f'                   | Nearest  | Farthest |')
+    print(f'    |--------------+---------------------|')
+    print(f'    | Cam distance | {d_min_str.center(w)} | {d_max_str.center(w)} |')
+    print(f'    | View angle   | {alpha_at_min_str.center(8)} | {alpha_at_max_str.center(w)} |')
+    print(f'    | Tangent elev | {elev_min_str.center(w)} | {elev_max_str.center(w)} |')
+    print(f'    |--------------+---------------------|')
     print('=' * 80)
-    print('''For reference:
-    A 10mm lens on APS-C 1.5x crop sensor with 16:9 crop in a 3:2 camera
-    results in 68.04° vertical angle of view.''')
+    print('''Camera max distance is such that the 45° elevation at the visible edge of the
+sphere fits exactly at the top of the video frame. Beyond that distance the 45°
+elevation will appear below the visible edge of the sphere in video.
+
+Camera min distance is such that the entire scene still fits vertically within
+the frame while the camera is safely outside the flight sphere.
+
+For reference:
+    * A 10mm lens on APS-C 1.5x crop sensor with 16:9 crop in a 3:2 camera
+      results in 68.04° vertical angle of view.
+    * A 14mm lens on full-frame sensor with 16:9 crop in a 3:2 camera
+      results in 71.75° vertical angle of view.''')
     print('=' * 80)
 
 
 if __name__ == '__main__':
-    main()
+    DEFAULT_R = 21.0  # default sphere radius
+    DEFAULT_C = -1.0  # default camera height relative to equator
+    DEFAULT_G = -1.5  # default ground level relative to equator
+    DEFAULT_A = 71.75  # default maximum vertical viewing angle of the camera, in degrees
+    AP = ap.ArgumentParser(description='Camera position calculator for VideoF2B application.')
+    AP.add_argument('-r', required=False, default=DEFAULT_R, type=float,
+                    help=f'Radius of sphere [default = {DEFAULT_R}]')
+    AP.add_argument('-c', required=False, default=DEFAULT_C, type=float,
+                    help=f'Height of camera relative to equator (up is positive) [default = {DEFAULT_C}]')
+    AP.add_argument('-g', required=False, default=DEFAULT_G, type=float,
+                    help=f'Height of ground level relative to equator (up is positive) [default = {DEFAULT_G}]')
+    AP.add_argument('-a', required=False, default=DEFAULT_A, type=float,
+                    help=f'Maximum vertical view angle of camera, in degrees [default = {DEFAULT_A}°]')
+    args = AP.parse_args()
+    main(R=args.r, h=args.c, max_alpha=args.a, ground_level=args.g)
