9091e3f8c22e3fb1bdb8a99288b083e419e8b44d
amv <basil96@users.noreply.github.com>
2021-07-09 10:19:32 -0400

    Conf file bugfixes
    * Fix unreferenced CF variable when we prompt user for all the things.
    
    * Add context managers around conf files.
    
    * Add exception handlers around conf file I/O.

--- Video.py
+++ Video.py
@@ -31,13 +31,13 @@ def LoadVideo(path=None):
     root.withdraw()  # use to hide tkinter window
     live = False
 
+    initialdir = '../'
     try:
-        CF = open('vid.conf', 'r')
-        initialdir = CF.read()
-        print(initialdir)
-        CF.close()
-    except:
-        initialdir = '../'
+        with open('vid.conf', 'r') as CF:
+            initialdir = CF.read()
+            print(initialdir)
+    except Exception as readErr:
+        print(f'Error reading vid.conf: {readErr}')
 
     if path is None:
         path = tkFileDialog.askopenfilename(parent=root, initialdir=initialdir,
@@ -47,17 +47,15 @@ def LoadVideo(path=None):
         if path:
             live = True
     else:
-        CF = open('vid.conf', 'w')
-        CF.write(os.path.dirname(path))
-        CF.close()
+        with open('vid.conf', 'w') as CF:
+            CF.write(os.path.dirname(path))
 
     print(f'Video path: {path}')
     if not path:
         return None, None, live
 
-    CF = open('vid.conf', 'w')
-    CF.write(os.path.dirname(path))
-    CF.close()
+    with open('vid.conf', 'w') as CF:
+        CF.write(os.path.dirname(path))
 
     cap = FileVideoStream(path).start()
     if not cap.isOpened():  # check if we succeeded
