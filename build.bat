2c331f446c8411f0aaa938b8c775233a19790f43
amv <basil96@users.noreply.github.com>
2021-09-24 14:28:19 -0400

    Clean up obsolete items
    * Get rid of all `requirements*.txt` files.
    
    * Move leftover dependencies from -dev as disabled placeholders in `setup.cfg`.
    
    * Remove obsolete shell build scripts.

--- build.bat
+++
@@ -1,9 +0,0 @@
-pyinstaller ^
---onefile ^
---clean ^
---hidden-import=platformdirs.windows ^
---exclude-module=matplotlib ^
---windowed ^
---add-data="resources;resources" ^
---version-file=version_win ^
-VideoF2B.py
